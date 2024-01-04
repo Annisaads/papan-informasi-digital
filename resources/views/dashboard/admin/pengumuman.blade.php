3b0d645edba31633683dd7890f8b40ab44516c6c
Putri Angel Saraswati <putriangel5946@gmail.com>
2024-01-04 08:18:51 +0700

    Memperbaiki pengumuman dan pesan tokoh

--- resources/views/dashboard/admin/pengumuman.blade.php
+++ resources/views/dashboard/admin/pengumuman.blade.php
@@ -1,28 +1,179 @@
 @extends('dashboard.layouts.app')
 
 @section('content')
-<div class="container-fluid">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title fw-semibold mb-4">Pengumuman</h5>
+    <div class="container-fluid">
         <div class="card">
-          <div class="card-body">
-            <form>
-              <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Tanggal Pengumuman</label>
-                <input type="date" id="disabledTextInput" class="form-control">
-              </div>
-              <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Isi Pengumuman</label>
-                <textarea type="text" id="disabledTextInput" class="form-control" placeholder="Tambahkan pengumuman" rows="5"></textarea>
-              </div>
-              <button type="submit" class="btn btn-primary">Simpan</button>
-            </form>
-          </div>
+            <div class="card-body">
+                <h5 class="card-title fw-semibold mb-4">Input Pengumuman</h5>
+                <div class="card">
+                    <div class="card-body">
+                        <form>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Tanggal Pengumuman</label>
+                                <input type="date" id="disabledTextInput" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Isi Pengumuman</label>
+                                <textarea type="text" id="disabledTextInput" class="form-control" placeholder="Tambahkan pengumuman" rows="5"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Simpan</button>
+                        </form>
+                    </div>
+                </div>
+                <h5 class="card-title fw-semibold mb-4">Form Pengumuman</h5>
+                <div class="table-responsive" style="max-height: 400px;">
+                    <table class="table text-nowrap mb-0 align-middle">
+                        <thead class="text-dark fs-4">
+                            <tr>
+                                <th class="border-bottom-0">
+                                    <h6 class="fw-semibold mb-0"></h6>
+                                </th>
+                                <th class="border-bottom-0">
+                                    <h6 class="fw-semibold mb-0" style="color: #5d87ff;">Tanggal</h6>
+                                </th>
+                                <th class="border-bottom-0">
+                                    <h6 class="fw-semibold mb-0" style="color: #5d87ff;">Isi</h6>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="border-bottom-0">
+                                    <div class="d-flex align-items-center gap-2">
+                                        <button style="background-color: #ffff;border: none;">
+                                            <img src="/dashboard/assets/images/logos/trash.png" width="20"
+                                                alt="">
+                                        </button>
+                                    </div>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <p class="mb-0 fw-normal">04/01/2024</p>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <h6 class="fw-semibold mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                        sed do
+                                        eiusmod tempor incididunt ut labore Ut enim ad minim veniam, quis nostrud
+                                        exercitation</h6>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="border-bottom-0">
+                                    <div class="d-flex align-items-center gap-2">
+                                        <button style="background-color: #ffff;border: none;">
+                                            <img src="/dashboard/assets/images/logos/trash.png" width="20"
+                                                alt="">
+                                        </button>
+                                    </div>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <p class="mb-0 fw-normal">04/01/2024</p>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <h6 class="fw-semibold mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                        sed do
+                                        eiusmod tempor incididunt ut labore Ut enim ad minim veniam, quis nostrud
+                                        exercitation</h6>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="border-bottom-0">
+                                    <div class="d-flex align-items-center gap-2">
+                                        <button style="background-color: #ffff;border: none;">
+                                            <img src="/dashboard/assets/images/logos/trash.png" width="20"
+                                                alt="">
+                                        </button>
+                                    </div>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <p class="mb-0 fw-normal">04/01/2024</p>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <h6 class="fw-semibold mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                        sed do
+                                        eiusmod tempor incididunt ut labore Ut enim ad minim veniam, quis nostrud
+                                        exercitation</h6>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="border-bottom-0">
+                                    <div class="d-flex align-items-center gap-2">
+                                        <button style="background-color: #ffff;border: none;">
+                                            <img src="/dashboard/assets/images/logos/trash.png" width="20"
+                                                alt="">
+                                        </button>
+                                    </div>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <p class="mb-0 fw-normal">04/01/2024</p>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <h6 class="fw-semibold mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                        sed do
+                                        eiusmod tempor incididunt ut labore Ut enim ad minim veniam, quis nostrud
+                                        exercitation</h6>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="border-bottom-0">
+                                    <div class="d-flex align-items-center gap-2">
+                                        <button style="background-color: #ffff;border: none;">
+                                            <img src="/dashboard/assets/images/logos/trash.png" width="20"
+                                                alt="">
+                                        </button>
+                                    </div>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <p class="mb-0 fw-normal">04/01/2024</p>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <h6 class="fw-semibold mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                        sed do
+                                        eiusmod tempor incididunt ut labore Ut enim ad minim veniam, quis nostrud
+                                        exercitation</h6>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="border-bottom-0">
+                                    <div class="d-flex align-items-center gap-2">
+                                        <button style="background-color: #ffff;border: none;">
+                                            <img src="/dashboard/assets/images/logos/trash.png" width="20"
+                                                alt="">
+                                        </button>
+                                    </div>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <p class="mb-0 fw-normal">04/01/2024</p>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <h6 class="fw-semibold mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                        sed do
+                                        eiusmod tempor incididunt ut labore Ut enim ad minim veniam, quis nostrud
+                                        exercitation</h6>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="border-bottom-0">
+                                    <div class="d-flex align-items-center gap-2">
+                                        <button style="background-color: #ffff;border: none;">
+                                            <img src="/dashboard/assets/images/logos/trash.png" width="20"
+                                                alt="">
+                                        </button>
+                                    </div>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <p class="mb-0 fw-normal">04/01/2024</p>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <h6 class="fw-semibold mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                        sed do
+                                        eiusmod tempor incididunt ut labore Ut enim ad minim veniam, quis nostrud
+                                        exercitation</h6>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-
 @endsection
-
--- resources/views/dashboard/admin/pesan-tokoh.blade.php
+++ resources/views/dashboard/admin/pesan-tokoh.blade.php
@@ -4,7 +4,7 @@
     <div class="container-fluid">
         <div class="card">
             <div class="card-body">
-                <h5 class="card-title fw-semibold mb-4">Pesan Tokoh</h5>
+                <h5 class="card-title fw-semibold mb-4">Input Pesan Tokoh</h5>
                 <div class="card">
                     <div class="card-body">
                         <form>
@@ -26,6 +26,188 @@
                             <button type="submit" class="btn btn-primary">Simpan</button>
                         </form>
                     </div>
+                </div>
+                <h5 class="card-title fw-semibold mb-4">Form Pesan Tokoh</h5>
+                <div class="table-responsive" style="max-height: 400px;">
+                    <table class="table text-nowrap mb-0 align-middle">
+                        <thead class="text-dark fs-4">
+                            <tr>
+                                <th class="border-bottom-0">
+                                    <h6 class="fw-semibold mb-0"></h6>
+                                </th>
+                                <th class="border-bottom-0">
+                                    <h6 class="fw-semibold mb-0" style="color: #5d87ff;">Nama</h6>
+                                </th>
+                                <th class="border-bottom-0">
+                                    <h6 class="fw-semibold mb-0" style="color: #5d87ff;">Jabatan</h6>
+                                </th>
+                                <th class="border-bottom-0">
+                                    <h6 class="fw-semibold mb-0" style="color: #5d87ff;">Pesan</h6>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="border-bottom-0">
+                                    <div class="d-flex align-items-center gap-2">
+                                        <button style="background-color: #ffff;border: none;">
+                                            <img src="/dashboard/assets/images/logos/trash.png" width="20"
+                                                alt="">
+                                        </button>
+                                    </div>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <p class="mb-0 fw-normal">Yang Jungwon</p>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <p class="mb-0 fw-normal">Enhypen</p>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <h6 class="fw-semibold mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                        sed do
+                                        eiusmod tempor incididunt ut labore Ut enim ad minim veniam, quis nostrud
+                                        exercitation
+                                    </h6>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="border-bottom-0">
+                                    <div class="d-flex align-items-center gap-2">
+                                        <button style="background-color: #ffff;border: none;">
+                                            <img src="/dashboard/assets/images/logos/trash.png" width="20"
+                                                alt="">
+                                        </button>
+                                    </div>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <p class="mb-0 fw-normal">Annisa Jamal</p>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <p class="mb-0 fw-normal">Staf</p>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <h6 class="fw-semibold mb-0">Bau apa yang romantis??? AMISyuu hehehehehehe kamu ga
+                                        kangen
+                                        aku???</h6>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="border-bottom-0">
+                                    <div class="d-flex align-items-center gap-2">
+                                        <button style="background-color: #ffff;border: none;">
+                                            <img src="/dashboard/assets/images/logos/trash.png" width="20"
+                                                alt="">
+                                        </button>
+                                    </div>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <p class="mb-0 fw-normal">Angel</p>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <p class="mb-0 fw-normal">Figuran</p>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <h6 class="fw-semibold mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                        sed do
+                                        eiusmod tempor incididunt ut labore Ut enim ad minim veniam, quis nostrud
+                                        exercitation
+                                    </h6>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="border-bottom-0">
+                                    <div class="d-flex align-items-center gap-2">
+                                        <button style="background-color: #ffff;border: none;">
+                                            <img src="/dashboard/assets/images/logos/trash.png" width="20"
+                                                alt="">
+                                        </button>
+                                    </div>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <p class="mb-0 fw-normal">Putri A</p>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <p class="mb-0 fw-normal">Cedit cewe adit</p>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <h6 class="fw-semibold mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                        sed do
+                                        eiusmod tempor incididunt ut labore Ut enim ad minim veniam, quis nostrud
+                                        exercitation
+                                    </h6>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="border-bottom-0">
+                                    <div class="d-flex align-items-center gap-2">
+                                        <button style="background-color: #ffff;border: none;">
+                                            <img src="/dashboard/assets/images/logos/trash.png" width="20"
+                                                alt="">
+                                        </button>
+                                    </div>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <p class="mb-0 fw-normal">Jakik</p>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <p class="mb-0 fw-normal">Pengcarry Puh</p>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <h6 class="fw-semibold mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                        sed do
+                                        eiusmod tempor incididunt ut labore Ut enim ad minim veniam, quis nostrud
+                                        exercitation
+                                    </h6>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="border-bottom-0">
+                                    <div class="d-flex align-items-center gap-2">
+                                        <button style="background-color: #ffff;border: none;">
+                                            <img src="/dashboard/assets/images/logos/trash.png" width="20"
+                                                alt="">
+                                        </button>
+                                    </div>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <p class="mb-0 fw-normal">Mas Boy</p>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <p class="mb-0 fw-normal">Ketua Himpunan Gym L1</p>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <h6 class="fw-semibold mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                        sed do
+                                        eiusmod tempor incididunt ut labore Ut enim ad minim veniam, quis nostrud
+                                        exercitation
+                                    </h6>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="border-bottom-0">
+                                    <div class="d-flex align-items-center gap-2">
+                                        <button style="background-color: #ffff;border: none;">
+                                            <img src="/dashboard/assets/images/logos/trash.png" width="20"
+                                                alt="">
+                                        </button>
+                                    </div>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <p class="mb-0 fw-normal">Sandhika Galih</p>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <p class="mb-0 fw-normal">Kaka Mentor</p>
+                                </td>
+                                <td class="border-bottom-0">
+                                    <h6 class="fw-semibold mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                        sed do
+                                        eiusmod tempor incididunt ut labore Ut enim ad minim veniam, quis nostrud
+                                        exercitation
+                                    </h6>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
