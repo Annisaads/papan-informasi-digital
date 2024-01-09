92bf196c5eaf986db0e07cdf9c57607f4bab41b6
Annisaads <annisa.dwi.septiani03@gmail.com>
2024-01-09 08:47:21 +0700

    memperbaiki dashboard admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,8 +8,10 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
+        $jumlahFoto = Profile::count();
+        $jumlahVideo = Profile::count();
         $jumlahProfil = Profile::count();
-        return view("dashboard.admin.index", compact('jumlahProfil'));
+        return view("dashboard.admin.index", compact('jumlahProfil', 'jumlahVideo', 'jumlahFoto'));
     }
 
 }
