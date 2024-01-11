6db0e6dc5173c21f1fcc96c29e0058726b8b3087
Annisaads <annisa.dwi.septiani03@gmail.com>
2024-01-11 16:25:58 +0700

    memperbaiki tabel announcements

--- database/migrations/2024_01_11_022349_create_announcements_table.php
+++ database/migrations/2024_01_11_022349_create_announcements_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('announcements', function (Blueprint $table) {
             $table->id();
             $table->string('announcement')->nullable();
-            $table->date('date')->nullable();
             $table->timestamps();
         });
     }
