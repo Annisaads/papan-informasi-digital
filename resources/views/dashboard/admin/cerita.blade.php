c12ed7ebb1a819db9542bead86aaf5f4fc28387d
Putri Angel Saraswati <putriangel5946@gmail.com>
2024-01-17 14:40:13 +0700

    Menambahkan fitur cerita

---
+++ resources/views/dashboard/admin/cerita.blade.php
@@ -0,0 +1,70 @@
+@extends('dashboard.layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title fw-semibold mb-4">Input Cerita</h5>
+                <div class="card">
+                    <div class="card-body">
+                        @if (session()->has('success'))
+                            <div class="alert alert-success alert-dismissible fade show mt-3">
+                                <strong>Berhasil!</strong> {{ session('success') }}
+                            </div>
+                        @endif
+                        @if (session()->has('delete-success'))
+                            <div class="alert alert-success alert-dismissible fade show mt-3">
+                                <strong>Berhasil!</strong> {{ session('delete-success') }}
+                            </div>
+                        @endif
+                        <form action="{{ route('photo.create') }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Foto</label>
+                                <input type="file" name="photo" id="disabledTextInput" class="form-control"></input>
+                                @error('photo')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <button type="submit" class="btn btn-primary">Simpan</button>
+                        </form>
+                    </div>
+                </div>
+                @if ($photo->isNotEmpty())
+                    <h5 class="card-title mb-9 fw-semibold">Form Cerita</h5>
+                    <div class="table-responsive" style="max-height: 300px;">
+                        <table class="table text-nowrap mb-0 align-middle">
+                            <thead class="text-dark fs-4">
+                            </thead>
+                            <tbody>
+                                @foreach ($photo as $photo)
+                                    <tr>
+                                        <td class="border-bottom-0" style="width: 5%;">
+                                            <div class="d-flex align-items-center gap-2">
+                                                <form action="{{ route('photo.delete', $photo->id) }}" method="POST">
+                                                    @csrf
+                                                    @method('GET')
+                                                    <button type="submit" style="background-color: #ffff; border: none;">
+                                                        <img src="/dashboard/assets/images/logos/trash.png" width="20"
+                                                            alt="">
+                                                    </button>
+                                                </form>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <img src="{{ asset('storage') . '/' . $photo->photo }}"
+                                                alt="{{ $photo->photo }}" width="120">
+                                        </td>
+                                        <td class="border-bottom-0" style="width: 95%;">
+                                            <h6 class="fw-semibold mb-0">{{ $photo->photo }}</h6>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/dashboard/partials/sidebar.blade.php
+++ resources/views/dashboard/partials/sidebar.blade.php
@@ -1,121 +1,84 @@
 <aside class="left-sidebar">
     <!-- Sidebar scroll-->
     <div>
-      <div
-        class="brand-logo d-flex align-items-center justify-content-between"
-      >
-        <a href="./index.html" class="text-nowrap logo-img">
-          <img
-            src="{{asset('dashboard/assets/images/logos/logo-ptsb.png')}}"
-            width="200"
-            alt=""
-          />
-        </a>
-        <div
-          class="close-btn d-xl-none d-block sidebartoggler cursor-pointer"
-          id="sidebarCollapse"
-        >
-          <i class="ti ti-x fs-8"></i>
+        <div class="brand-logo d-flex align-items-center justify-content-between">
+            <a href="./index.html" class="text-nowrap logo-img">
+                <img src="{{ asset('dashboard/assets/images/logos/logo-ptsb.png') }}" width="200" alt="" />
+            </a>
+            <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+                <i class="ti ti-x fs-8"></i>
+            </div>
         </div>
-      </div>
-      <!-- Sidebar navigation-->
-      <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
-        <ul id="sidebarnav">
-          <li class="nav-small-cap">
-            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-            <span class="hide-menu">Halaman Utama</span>
-          </li>
-          <li class="sidebar-item">
-            <a
-              class="sidebar-link"
-              href="{{route('dashboard.index')}}"
-              aria-expanded="false"
-            >
-              <span>
-                <i class="ti ti-layout-dashboard"></i>
-              </span>
-              <span class="hide-menu">Beranda</span>
-            </a>
-          </li>
-          <li class="nav-small-cap">
-            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-            <span class="hide-menu">Form</span>
-          </li>
-          <li class="sidebar-item">
-            <a
-              class="sidebar-link"
-              href="{{route('profile.index')}}"
-              aria-expanded="false"
-            >
-              <span>
-                <i class="ti ti-file-description"></i>
-              </span>
-              <span class="hide-menu">Profil</span>
-            </a>
-          </li>
-          <li class="sidebar-item">
-            <a
-              class="sidebar-link"
-              href="{{route('video.index')}}"
-              aria-expanded="false"
-            >
-              <span>
-                <i class="ti ti-cards"></i>
-              </span>
-              <span class="hide-menu">Video</span>
-            </a>
-          </li>
-          <li class="sidebar-item">
-            <a
-              class="sidebar-link"
-              href="{{route('photo.index')}}"
-              aria-expanded="false"
-            >
-              <span>
-                <i class="ti ti-cards"></i>
-              </span>
-              <span class="hide-menu">Foto</span>
-            </a>
-          </li>
-          <li class="sidebar-item">
-            <a
-              class="sidebar-link"
-              href="{{route('announcement.index')}}"
-              aria-expanded="false"
-            >
-              <span>
-                <i class="ti ti-alert-circle"></i>
-              </span>
-              <span class="hide-menu">Pengumuman</span>
-            </a>
-          </li>
-          <li class="sidebar-item">
-            <a
-              class="sidebar-link"
-              href="{{route('quote.index')}}"
-              aria-expanded="false"
-            >
-              <span>
-                <i class="ti ti-article"></i>
-              </span>
-              <span class="hide-menu">Pesan Tokoh</span>
-            </a>
-          </li>
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+            <ul id="sidebarnav">
+                <li class="nav-small-cap">
+                    <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                    <span class="hide-menu">Halaman Utama</span>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="{{ route('dashboard.index') }}" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-layout-dashboard"></i>
+                        </span>
+                        <span class="hide-menu">Beranda</span>
+                    </a>
+                </li>
+                <li class="nav-small-cap">
+                    <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                    <span class="hide-menu">Form</span>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="{{ route('video.index') }}" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-cards"></i>
+                        </span>
+                        <span class="hide-menu">Video</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="{{ route('photo.index') }}" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-cards"></i>
+                        </span>
+                        <span class="hide-menu">Foto</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="{{ route('cerita.index') }}" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-cards"></i>
+                        </span>
+                        <span class="hide-menu">Cerita</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="{{ route('profile.index') }}" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-article"></i>
+                        </span>
+                        <span class="hide-menu">Pengumuman</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="{{ route('quote.index') }}" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-article"></i>
+                        </span>
+                        <span class="hide-menu">Berita</span>
+                    </a>
+                </li>
 
-           <li class="nav-small-cap">
-            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-            <span class="hide-menu"></span>
-          </li>
-          <li class="sidebar-item">
-            <a
-              href="{{route('logout')}}"
-              class="btn btn-outline-primary mx-3 mt-2 d-block"
-              >Keluar</a
-            >
-          </li>
-        </ul>
-      </nav>
-      <!-- End Sidebar navigation -->
+                <li class="nav-small-cap">
+                    <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                    <span class="hide-menu"></span>
+                </li>
+                <li class="sidebar-item">
+                    <a href="{{ route('logout') }}" class="btn btn-outline-primary mx-3 mt-2 d-block">Keluar</a>
+                </li>
+            </ul>
+        </nav>
+        <!-- End Sidebar navigation -->
     </div>
     <!-- End Sidebar scroll-->
-  </aside>
+</aside>
