fb38b4ae99f8e1c6f513e62b1efd6484a2cd1e0b
Putri Angel Saraswati <putriangel5946@gmail.com>
2024-01-19 09:56:54 +0700

    Memperbaiki halaman masuk dan profil, membuat halaman daftar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -60,8 +60,16 @@
                                             <div class="invalid-feedback">{{ $message }}</div>
                                         @enderror
                                     </div>
+                                    <div class="mb-4" style="text-align: right">
+                                        <a class="text-primary fw-bold" href="./index.html">Lupa Password?</a>
+                                    </div>
                                     <button type="submit"
                                         class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Masuk</button>
+                                    <div class="d-flex align-items-center justify-content-center">
+                                        <p class="fs-4 mb-0 fw-bold">Belum punya akun?</p>
+                                        <a class="text-primary fw-bold ms-2"
+                                            href="./authentication-register.html">Daftar</a>
+                                    </div>
                                 </form>
                             </div>
                         </div>
